23a957d7ebf05978a7d02ea26167de0f44771b64
Thomas D. Pellegrin <contact@theaviationdoctor.com>
2020-10-27 16:11:30 +0800

    Taken from https://data.giss.nasa.gov/gistemp/graphs/graph_data/Global_Mean_Estimates_based_on_Land_and_Ocean_Data/graph.txt on 2020-10-27

--- scripts/gmst.R
+++ scripts/gmst.R
@@ -12,6 +12,7 @@ library(tidyverse)
 url <- "https://raw.githubusercontent.com/TheAviationDoctor/ClimateChangeTakeoffPerformance/main/data/land-ocean-temperature-index-1880-2019.csv"
 #colnames <- c("Year", "Unsmoothed", "Smoothed")
 coltypes <- list(col_character(), col_double(), col_double())
-gmst <- read_csv(url, col_names = TRUE, col_types = coltypes, skip = 5)
+gmst <- read_csv(url, col_names = TRUE, col_types = coltypes, skip = 1)
 gmst
 
+hist(gmst$Year, gmst$Unsmoothed)
